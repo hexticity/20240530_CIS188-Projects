4366bd83d92303d2cfd5a615f0acd368115e823f
hexticity <159094903+hexticity@users.noreply.github.com>
2024-05-30 23:49:02 -0700

    Create Lab01_PythonBasics.py

---
+++ Lab01_PythonBasics.py
@@ -0,0 +1,31 @@
+"""
+Author: Raymond Llamas
+Course: PCC - CIS188 - Scripting for Automation
+Instructor: P. Farmer
+Description: The following program will ask the user three questions, output the users answers, and output a final
+message that possesses a newly calculated age based on a designed equation (length of name + inputted age = new age)
+"""
+
+# Function to get user input and print the desired output
+def main():
+    # Asking for the user's name
+    name = input("What is your name? ")
+    
+    # Greeting the user
+    print(f"Hello, {name}")
+    
+    # Asking for the user's age
+    age = int(input("What is your age? "))
+
+    # Calculate the length of the name
+    name_len = len(name)
+    
+    # Calculating the new age based on the length of the name
+    new_age = age + name_len
+    
+    # Printing the final message
+    print(f"{name}, if we add the number of letters in your name to your age then you will be {new_age} in {name_len} years.")
+
+# Calling the main function
+if __name__ == "__main__":
+    main()
