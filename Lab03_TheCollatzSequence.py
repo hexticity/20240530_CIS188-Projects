7f46abe124f3adf8d0aa6d105fbfb4789c0c632f
hexticity <159094903+hexticity@users.noreply.github.com>
2024-06-17 17:26:11 -0700

    Create Lab03_TheCollatzSequence.py

---
+++ Lab03_TheCollatzSequence.py
@@ -0,0 +1,14 @@
+"""
+Author: Raymond Llamas
+Course: PCC - CIS188 - Scripting for Automation
+Instructor: P. Farmer
+Description: 
+This program defines a function named collatz() that performs the Collatz sequence operation.
+If the number is even, it returns number // 2.
+If the number is odd, it returns 3 * number + 1.
+"""
+
+def collatz(number):
+  # Check if the inputted number is even
+  if number % 2 == 0:
+    result = number // 2
